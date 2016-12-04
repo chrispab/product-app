d8cd6a5946737688f1965783914cad90ba083869
chrispab <cbattisson@gmail.com>
2016-12-04 20:09:15 +0000

    more clean2

--- public/index.php
+++ public/index.php
@@ -10,15 +10,8 @@ require_once ('Router.php');
  * Primary file. Point of entry for the ap.
  *
  * Index.php intercepts all requests from the client.
- * Includes primary productsController, creates it and passes web requests to
+ * Includes primary router, creates it and passes web requests to
  * it for further routing and processing
  */
-
-// require_once(__DIR__."/../controller/ProductsController.php");
-//
-// $controller = new ProductsController();
-// $controller->handleRequest();
-
-
 $app = new Router();
 $app->handleRequest();
